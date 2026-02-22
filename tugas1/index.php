6a1595eb89d497952bae4bd1eb3bc5619a903419
kresnawan <sanjayakresnawan@gmail.com>
2026-02-22 23:09:37 +0700

    Initial commit

---
+++ tugas1/index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>255150400111034 - Kresnawan Restu Sanjaya</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <div class="wrap">
+            <div class="section-first">
+                <h2>
+                    Kresnawan Restu Sanjaya
+                </h2>
+
+                <p class="ket">255150400111034</p>
+
+                <p style="margin-top: 24px;">
+                    Saya merupakan perancang perangkat lunak yang telah
+                    di industri selama kurang lebih 4 tahun. Dengan kemampuan
+                    yang saya punya, saya memahami baik mikro maupun makro
+                    dari pengembangan perangkat lunak terutama berbasis sistem
+                    dan berbasis web. Serta analisis kebutuhan organisasi maupun
+                    bisnis dalam ekspansi penggunaan teknologi informasi.
+                </p>
+            </div>
+
+            <div class="section">
+                <h2>
+                    Riwayat Pendidikan
+                </h2>
+                <div class="section-item">
+                    <h3>Universitas Brawijaya</h3>
+                    <p class="ket">S1 Sistem Informasi</p>
+                    <p>2025 - sekarang</p>
+                    <p class="desc">
+                        Saya mengenyam pendidikan S1 Sistem Informasi di
+                        Fakultas Ilmu Komputer, Universitas Brawijaya.
+                        Menempa keahlian saya dalam melihat permasalahan
+                        teknologi informasi suatu organisasi dari perspektif
+                        yang lebih objektif.
+                        Memastikan produk teknologi informasi yang tepat guna,
+                        dan berkualitas.
+                    </p>
+                </div>
+                <div class="section-item">
+                    <h3>SMA Negeri 2 Pare</h3>
+                    <p class="ket">Sains teknik</p>
+                    <p>2022 - 2025</p>
+                    <p class="desc">
+                        Dengan latar belakang saya dari penjurusan sains teknik, saya
+                        telah menguasai pemahaman mengenai pemecahan masalah kompleks
+                        menggunakan pendekatan yang paling rasional dan dapat diandalkan.
+                    </p>
+                </div>
+            </div>
+
+            <div class="section">
+                <h2>
+                    Pengalaman dan Pengamalan
+                </h2>
+
+                <div class="section-item">
+                    <h3>Publikasi Jurnal: PengabdianMu</h3>
+                    <p class="ket">Anggota</p>
+                    <p>2025</p>
+
+                    <p class="desc">
+                        Saya berkontribusi pada pembuatan sekaligus pengembangan dan publikasi jurnal pengabdian masyarakat
+                        yang dipelopori oleh tim peneliti dari Fakultas Ilmu Sosial dan
+                        Ilmu Politik, Universitas Brawijaya. Khususnya, pada bagian
+                        pengembangan aplikasi
+                        <span style="font-style: italic;">geo-spatial</span>
+                        berbasis web untuk menampilkan peta persebaran
+                        tempat bersejarah di daerah Singosari, Kabupaten Malang. Lampiran
+                        <a href="https://journal.umpr.ac.id/index.php/pengabdianmu/">dapat diakses disini.</a>
+                    </p>
+                </div>
+
+                <div class="section-item">
+                    <h3>MPK SMA Negeri 2 Pare</h3>
+                    <p class="ket">Sekretaris Umum</p>
+                    <p>2022 - 2024</p>
+
+                    <p class="desc">
+                        Dengan pengalaman saya mengikuti organisasi semasa SMA
+                        selama 2 periode, saya terbiasa
+                        untuk berkoordinasi, dan bekerja sama bersama tim. Menjunjung tinggi
+                        kebersamaan dan loyalitas.
+                    </p>
+                </div>
+            </div>
+
+            <div class="section">
+                <h2>
+                    Kontak
+                </h2>
+
+                <p style="margin-top: 24px;">Email: kresnawan@student.ub.ac.id</p>
+                <p>LinkedIn: <a href="https://www.linkedin.com/in/kresnawan/">https://www.linkedin.com/in/kresnawan/</a></p>
+            </div>
+        </div>
+    </body>
+</html>
