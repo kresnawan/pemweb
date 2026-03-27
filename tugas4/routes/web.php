df12dfad24802df9122ee302224fcb3424cd866e
kresnawan <sanjayakresnawan@gmail.com>
2026-03-27 11:08:55 +0700

    Serve basic routing

--- tugas4/routes/web.php
+++ tugas4/routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/", function () {
+    $logs = DB::table("log_keuangan")->get();
+
+    return view("welcome", [
+        "logs" => $logs,
+    ]);
+});
+
+Route::get("/tambah", function () {
+    return view("tambah");
 });
